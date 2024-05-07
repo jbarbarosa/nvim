fd885662cee84394fbc30411593848b8174fb5b6
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2024-05-07 07:14:39 -0300

    format on save

--- lua/user/none-ls.lua
+++ lua/user/none-ls.lua
@@ -5,11 +5,12 @@ local M = {
   }
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 function M.config()
   local null_ls = require "null-ls"
 
   local formatting = null_ls.builtins.formatting
-  local diagnostics =  null_ls.builtins.diagnostics
 
   null_ls.setup {
     debug = false,
@@ -22,11 +23,36 @@ function M.config()
       --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
       -- },
       -- formatting.eslint,
-      null_ls.builtins.diagnostics.flake8,
-      -- diagnostics.flake8,
       null_ls.builtins.completion.spell,
+      null_ls.builtins.formatting.gofmt,
+      -- null_ls.builtins.completion.gofmt,
     },
+   on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end
+	end,
   }
 end
+
+-- M.on_attach = function(client, bufnr)
+--   if client.supports_method("textDocument/formatting") then
+--     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--     vim.api.nvim_create_autocmd("BufWritePre", {
+--       group = augroup,
+--       buffer = bufnr,
+--       callback = function()
+--         vim.lsp.buf.formatting_sync()
+--       end,
+--     })
+--   end
+-- end
 
 return M
