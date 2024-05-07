d9a5a93dc031ecd7413729e033573d532100401a
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2024-05-07 07:14:21 -0300

    add neotest support for go, remove for unused

--- lua/user/neotest.lua
+++ lua/user/neotest.lua
@@ -1,17 +1,14 @@
 local M = {
   "nvim-neotest/neotest",
   dependencies = {
+    "nvim-neotest/nvim-nio",
     "nvim-treesitter/nvim-treesitter",
     -- general tests
     "vim-test/vim-test",
     "nvim-neotest/neotest-vim-test",
     -- language specific tests
-    "marilari88/neotest-vitest",
-    "nvim-neotest/neotest-python",
     "nvim-neotest/neotest-plenary",
-    "rouge8/neotest-rust",
-    "lawrence-laz/neotest-zig",
-    "rcasia/neotest-bash",
+    -- "/nvim-neotest/neotest-go",
   },
 }
 
@@ -28,11 +25,6 @@ function M.config()
   ---@diagnostic disable: missing-fields
   require("neotest").setup {
     adapters = {
-      require "neotest-python" {
-        dap = { justMyCode = false },
-      },
-      require "neotest-vitest",
-      require "neotest-zig",
       require "neotest-vim-test" {
         ignore_file_types = { "python", "vim", "lua", "javascript", "typescript" },
       },
