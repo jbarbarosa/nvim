9248cd193f4b64579168043da6164f87a2a7a808
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2024-05-21 21:09:47 -0300

    layout for dap-ui

--- lua/user/dap-ui.lua
+++ lua/user/dap-ui.lua
@@ -7,7 +7,46 @@ local M = {
 }
 
 function M.config()
-  require("dapui").setup()
+  require("dapui").setup({
+    layouts = {
+      {
+        elements = {
+          {
+            id = 'scopes',
+            size = 0.25,
+          },
+          {
+            id = 'watches',
+            size = 0.25,
+          },
+          {
+            id = 'breakpoints',
+            size = 0.25,
+          },
+          {
+            id = 'stacks',
+            size = 0.25,
+          },
+        },
+        position = 'right',
+        size = 0.28,
+      },
+      {
+        elements = {
+          {
+            id = 'repl',
+            size = 0.5,
+          },
+          {
+            id = 'console',
+            size = 0.5,
+          },
+        },
+        position = 'bottom',
+        size = 0.3,
+      },
+    },
+  })
 end
 
 return M
