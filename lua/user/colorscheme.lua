7e4d37005b93ff6f06b6683d20f4f539734c27eb
JP Barbosa <barbosa.swe@proton.me>
2026-06-17 11:16:08 -0300

    add transparency to colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,5 +3,7 @@ return {
 	name = "rose-pine-moon",
 	config = function()
 		vim.cmd("colorscheme rose-pine-moon")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end
 }
